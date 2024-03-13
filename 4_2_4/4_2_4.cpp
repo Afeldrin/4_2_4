188da1862d0391575213d64c324df01171a390fe
Alexander <shalagin.aiu@phystech.edu>
2024-03-13 20:29:09 +0300

    Добавьте файлы проекта.

---
+++ 4_2_4/4_2_4.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+void brackets(string& s) {
+
+    if (s == "") {
+        cout << "There is no sequence!" << endl;
+        return;
+    }
+
+    stack<char> st = {};
+
+    for (auto i = s.begin(); i != s.end(); ++i) {
+        if (*i == '(' or *i == '{' or *i == '[') {
+            st.push(*i);
+        }
+
+        else if (*i == ')' or *i == '}' or *i == ']') {
+
+            if (st.empty() == true) {
+                cout << "Sequence is incorrect!" << endl;
+                return;
+            };
+
+            if (*i == ')' and st.top() == '(') {
+                st.pop();
+            }
+            else if (*i == ')' and st.top() != '(') {
+                cout << "Sequence is incorrect!" << endl;
+                return;
+            }
+            if (*i == ']' and st.top() == '[') {
+                st.pop();
+            }
+            else if (*i == ']' and st.top() != '[') {
+                cout << "Sequence is incorrect!" << endl;
+                return;
+            }
+            if (*i == '}' and st.top() == '{') {
+                st.pop();
+            }
+            else if (*i == '}' and st.top() != '{') {
+                cout << "Sequence is incorrect!" << endl;
+                return;
+            }
+        }
+    }
+
+    if (st.empty() == true) {
+        cout << "Sequence is correct!" << endl;
+    }
+    else {
+        cout << "Sequence is incorrect!" << endl;
+    }
+
+    return;
+}
+
+int main()
+{
+    string str = "[)]";
+
+    brackets(str);
+
+    return 0;
+}
